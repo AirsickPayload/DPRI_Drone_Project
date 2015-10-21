4729e64c753014719e0c7c4cdbc423c893af3a8d
Alan Matuszczak <al4n00@gmail.com>
2015-10-21 21:13:29 +0200

    Dodanie okresowego sprawdzania polaczenia z klientem sterujacym po pewnym czasie nieaktywnosci

--- server.py
+++ server.py
@@ -1,57 +1,111 @@
 import socket
 import sys
+import time
+import thread
 import os
 from subprocess import call
+clientPort = 8888
+serverPort = 8887
+pingPort = 8889
+awaiting = closed = emergency = False
+lastThrottleValue = None
+throttleEmergencyMinVal = 25
 
-PORT = 8887
+def emergencyDownThrottling(placeholder1, placeholder2):
+    #1=XX%
+    split = lastThrottleValue.split('=')
+    throttlePin = split[0]
+    throttleVal = split[1].split('%')
+    throttleVal = int(throttleVal[0])
+    while throttleVal > throttleEmergencyMinVal:
+        throttleVal -= 5
+        call('echo ' + throttlePin + '=' +  + ' > ' + '/dev/servoblaster', shell=True)
+        sleep(0.5)
+        time.sleep(0.3)
+
+def pingThreadMethod(addr, ignored):
+    pingsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        pingsocket.bind(('', pingPort)) # zamiast socket.gethostname() mozna '', zeby byl osiagalny ze wszystkich interfejsow
+    except socket.error as msg:
+        print 'PingSocket bind failed. Error code: ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
+    #oczekiwanie na odp. ze strony klienta nie moze trwac wiecznie
+    pingsocket.settimeout(2) # max 2s
+    while 1:
+        time.sleep(1.5)
+        try:
+            if(awaiting):
+                print 'PING?'
+                pingsocket.sendto('PING?', (addr, 8889))
+                pingData, addr2 = pingsocket.recvfrom(1024)
+                pingData = pingData.decode()
+
+                if pingData == 'PONG!':
+                    print ' PONG!'
+                    continue
+                else:
+                    print 'ERROR: ' + pingData
+                    emergency = True
+                    break
+            if(closed):
+                closed = False
+                break
+        except socket.error as msg:
+            emergency = True
+            thread.start_new_thread(emergencyDownThrottling, ('',''))
+            print 'PingSocket timeout - EMERGENCY!'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print 'Socket created'
 
-# Bind socket
 try:
-    s.bind(('', PORT)) # zamiast socket.gethostname() mozna '', zeby byl osiagalny ze wszystkich interfejsow
+    s.bind(('', serverPort)) # zamiast socket.gethostname() mozna '', zeby byl osiagalny ze wszystkich interfejsow
 except socket.error as msg:
     print 'Bind failed. Error code: ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
 
 print 'Socket bind complete'
 
-#now keep talking with the client
 while 1:
-    #wait to accept a connection - blocking call
     versionStringClient, addr = s.recvfrom(1024)
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
-
     #compatibility string
-    versionStringServer = 'v05/10/2015'
+    versionStringServer = 'v20/10/2015'
     versionStringClient = versionStringClient.decode()
     if versionStringClient == versionStringServer:
         print "VERSION MATCH!"
-        s.sendto('VERSION MATCH', (addr[0], 8888))
+        s.sendto('VERSION MATCH', (addr[0], clientPort))
+        thread.start_new_thread(pingThreadMethod, addr)
         while 1:
+            if emergency:
+                continue
+
+            awaiting = True
             data, addr = s.recvfrom(1024)
+            awaiting = False
+
             data = data.decode()
             #OBSLUGA SPECJALNYCH KOMEND
             if data == 'CONN_CLOSE':
                 print "CLIENT CLOSED CONNECTION\n"
-                break;
+                closed = True
+                break
 
             if data == versionStringServer:
                 print "CLIENT RECONNECTED!"
-                s.sendto('VERSION MATCH', (addr[0], 8888))
+                s.sendto('VERSION MATCH', (addr[0], clientPort))
+                thread.start_new_thread(pingThreadMethod, addr)
 
             #OBSLUGA STANDARDOWYCH WARTOSCI
-            #a='0=100%,1=55%,2=21%,6=99%'
-            #b=a.split(','); print b[0]
             values = data.split(',')
-            path = os.path.dirname(sys.argv[0]) + 'plik.txt'
-            for value in values:
-                call('echo ' + value + ' >> ' +  path, shell=True) #zmienic potem na wysylanie do urzadzenia!
+            #for value in values:
+                #call('echo ' + value + ' > ' + '/dev/servoblaster', shell=True)
+            lastThrottleValue = values[0]
             print values
-            s.sendto('RECV_OK', (addr[0], 8888))
+            s.sendto('RECV_OK', (addr[0], clientPort))
+
     else:
-        s.sendto('VERSION MISMATCH', (addr[0], 8888))
+        s.sendto('VERSION MISMATCH', (addr[0], clientPort))
         print "VERSION MISMATCH: " + versionStringClient
-        break;
 s.close()
